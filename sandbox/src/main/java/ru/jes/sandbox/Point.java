758b41080b22a10cd43e3a0e579800643ebbba41
GustaveGlotz <68445226+GustaveGlotz@users.noreply.github.com>
2020-07-30 23:55:36 +0300

    Вычисление расстояния при помощи метода в классе Point

--- sandbox/src/main/java/ru/jes/sandbox/Point.java
+++ sandbox/src/main/java/ru/jes/sandbox/Point.java
@@ -1,11 +1,15 @@
 package ru.jes.sandbox;
 
 public class Point {
-    public double x1;
-    public double x2;
-    public double y1;
-    public double y2;
+    public double x;
+    public double y;
 
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
 
-
+    public double distance(Point p2) {
+        return Math.sqrt((p2.x - this.x) * (p2.x - this.x) + (p2.y - this.y) * (p2.y - this.y));
+    }
 }
