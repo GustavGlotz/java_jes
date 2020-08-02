73d57df4dbe335693b4edf5149754b2de0b515f3
GustaveGlotz <68445226+GustaveGlotz@users.noreply.github.com>
2020-08-02 12:24:00 +0300

    Созднан общий базовый класс для тестов TestBase

--- addressbook-web-tests/src/test/java/ru/jes/addressbook/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/jes/addressbook/ContactDeletionTests.java
@@ -6,42 +6,12 @@ import org.testng.annotations.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class ContactDeletionTests {
-    private WebDriver wd;
-
-
-    @BeforeMethod(alwaysRun = true)
-    public void setUp() throws Exception {
-        wd = new FirefoxDriver();
-        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    }
+public class ContactDeletionTests extends TestBase {
 
     @Test
     public void testContactDeletion() throws Exception {
-        wd.get("http://localhost/addressbook/");
-        wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("admin");
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys("secret");
-        wd.findElement(By.xpath("//input[@value='Login']")).click();
-        wd.findElement(By.id("19")).click();
-        wd.findElement(By.xpath("//input[@value='Delete']")).click();
+        selectContactDelete();
+        confirmDeletion();
 
-    }
-
-    @AfterMethod(alwaysRun = true)
-    public void tearDown() throws Exception {
-        wd.quit();
-    }
-
-
-    private boolean isAlertPresent() {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
     }
 }
