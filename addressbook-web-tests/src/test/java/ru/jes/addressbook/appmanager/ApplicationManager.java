c77445d99f4561d1702f48dc166d9436de85300a
GustaveGlotz <68445226+GustaveGlotz@users.noreply.github.com>
2020-08-02 15:40:49 +0300

    Созданы специализированные классы-помощники NavigationHelper, GroupHelper ,ContactHelper, SessionHelper

--- addressbook-web-tests/src/test/java/ru/jes/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/jes/addressbook/appmanager/ApplicationManager.java
@@ -9,9 +9,10 @@ import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
     public WebDriver wd;
-
+    private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
     private ContactHelper contactHelper;
+    private SessionHelper sessionHelper;
 
     public void init() {
         wd = new FirefoxDriver();
@@ -19,29 +20,19 @@ public class ApplicationManager {
         wd.get("http://localhost/addressbook/");
         groupHelper = new GroupHelper(wd);
         contactHelper = new ContactHelper(wd);
-        login("admin", "secret");
+        navigationHelper = new NavigationHelper(wd);
+        sessionHelper = new SessionHelper(wd);
+        sessionHelper.login("admin", "secret");
     }
 
-    public void login(String username, String password) {
-        groupHelper.wd.findElement(By.name("user")).click();
-        groupHelper.wd.findElement(By.name("user")).clear();
-        groupHelper.wd.findElement(By.name("user")).sendKeys(username);
-        groupHelper.wd.findElement(By.name("pass")).clear();
-        groupHelper.wd.findElement(By.name("pass")).sendKeys(password);
-        groupHelper.wd.findElement(By.xpath("//input[@value='Login']")).click();
-    }
-
-    public void gotoGroupPage() {
-        groupHelper.wd.findElement(By.linkText("groups")).click();
-    }
 
     public void stop() {
-        groupHelper.wd.quit();
+        wd.quit();
     }
 
     public boolean isAlertPresent() {
         try {
-            groupHelper.wd.switchTo().alert();
+            wd.switchTo().alert();
             return true;
         } catch (NoAlertPresentException e) {
             return false;
@@ -54,5 +45,9 @@ public class ApplicationManager {
 
     public ContactHelper getContactHelper() {
         return contactHelper;
+    }
+
+    public NavigationHelper getNavigationHelper() {
+        return navigationHelper;
     }
 }
