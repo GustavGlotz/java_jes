f8705863a326aa74730280a2ee4f5eca8a2f7943
GustaveGlotz <68445226+GustaveGlotz@users.noreply.github.com>
2020-08-05 18:23:46 +0300

    Реализована проверка и обеспечение выполнения предусловий для удаления, модификации групп

--- addressbook-web-tests/src/test/java/ru/jes/addressbook/appmanager/BaseHelper.java
+++ addressbook-web-tests/src/test/java/ru/jes/addressbook/appmanager/BaseHelper.java
@@ -2,6 +2,7 @@ package ru.jes.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 
 public class BaseHelper {
@@ -26,6 +27,15 @@ public class BaseHelper {
             wd.switchTo().alert();
             return true;
         } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    public boolean isElementPresent(By by) {
+        try {
+            wd.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
             return false;
         }
     }
--- addressbook-web-tests/src/test/java/ru/jes/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/jes/addressbook/tests/GroupDeletionTests.java
@@ -1,12 +1,16 @@
 package ru.jes.addressbook.tests;
 
 import org.testng.annotations.*;
+import ru.jes.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
     @Test
     public void testGroupDeletion() throws Exception {
         app.getNavigationHelper().gotoGroupPage();
+        if (! app.getGroupHelper().isThereAGroup()) {
+        app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+        }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnToGroupPage();
