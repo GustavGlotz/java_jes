551ed28fab8f57c2211e41d922012fcee50c709f
GustaveGlotz <68445226+GustaveGlotz@users.noreply.github.com>
2020-08-18 16:59:33 +0300

    списки в тестах на удаление, модификации контактов

--- addressbook-web-tests/src/test/java/ru/jes/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/jes/addressbook/tests/ContactDeletionTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.jes.addressbook.model.ContactData;
 
+import java.util.List;
+
 public class ContactDeletionTests extends TestBase {
 
     @Test
@@ -11,11 +13,11 @@ public class ContactDeletionTests extends TestBase {
         if (! app.getContactHelper().isThereAContact()) {
             app.getContactHelper().createContact(new ContactData("Джордани", "Йованович", "BabaYaga@mail.com", "375291234567"));
         }
-        int before = app.getContactHelper().getContactCount();
-        app.getContactHelper().selectContact(before - 1);
+        List<ContactData> before = app.getContactHelper().getContactList();
+        app.getContactHelper().selectContact(before.size() - 1);
         app.getContactHelper().deleteSelectedContact();
-        int after = app.getContactHelper().getContactCount();
-        Assert.assertEquals(after, before - 1 );
+        List<ContactData> after = app.getContactHelper().getContactList();
+        Assert.assertEquals(after.size(), before.size() - 1 );
 
     }
 }
--- addressbook-web-tests/src/test/java/ru/jes/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/jes/addressbook/tests/ContactModificationTests.java
@@ -5,6 +5,8 @@ import org.testng.annotations.Test;
 import ru.jes.addressbook.appmanager.BaseHelper;
 import ru.jes.addressbook.model.ContactData;
 
+import java.util.List;
+
 public class ContactModificationTests extends TestBase {
 
     @Test
@@ -12,13 +14,13 @@ public class ContactModificationTests extends TestBase {
         if (! app.getContactHelper().isThereAContact()) {
             app.getContactHelper().createContact(new ContactData("Джордани", "Йованович", "BabaYaga@mail.com", "375291234567"));
         }
-        int before = app.getContactHelper().getContactCount();
-        app.getContactHelper().selectContact(before -1);
+        List<ContactData> before = app.getContactHelper().getContactList();
+        app.getContactHelper().selectContact(before.size() -1);
         app.getContactHelper().ContactModification();
         app.getContactHelper().fillContactForm(new ContactData("Джордани", "Йованович", "BabaYaga@mail.com", "375291234567"));
         app.getContactHelper().submitContactModification();
         app.getContactHelper().gotoHomePage();
-        int after = app.getContactHelper().getContactCount();
-        Assert.assertEquals(after, before );
+        List<ContactData> after = app.getContactHelper().getContactList();
+        Assert.assertEquals(after.size(), before.size() );
     }
 }
