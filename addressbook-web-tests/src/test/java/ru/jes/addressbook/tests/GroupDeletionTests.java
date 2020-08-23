4747c8eb3d4301fb706a4e20c18ce3436c070f2f
GustaveGlotz <68445226+GustaveGlotz@users.noreply.github.com>
2020-08-23 17:20:49 +0300

    множества set в тестах для модификации групп

--- addressbook-web-tests/src/test/java/ru/jes/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/jes/addressbook/tests/GroupDeletionTests.java
@@ -1,10 +1,10 @@
 package ru.jes.addressbook.tests;
 
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 import ru.jes.addressbook.model.GroupData;
 
-import java.util.List;
 import java.util.Set;
 
 public class GroupDeletionTests extends TestBase {
@@ -12,7 +12,7 @@ public class GroupDeletionTests extends TestBase {
     @BeforeMethod
     public void ensurePreconditions() {
         app.goTo().groupPage();
-        if (app.group().list().size() == 0) {
+        if (app.group().all().size() == 0) {
             app.group().create(new GroupData().withName("test1").withHeader("test2").withFooter("test3"));
         }
     }
